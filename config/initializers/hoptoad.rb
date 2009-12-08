2dd9f69b3d5f06be5997dc784aadb91ad5fb90c6
Steven Derocher <sderocher@thoughtbot.com>
2009-12-08 15:03:22 -0500

    hoptoad api

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,4 +1,4 @@
-if ENV['60fcc4988eac07fdaf5aed052aab45c6e8eaadc6']
+if ENV['HOPTOAD_API_KEY']
   HoptoadNotifier.configure do |config|
     config.api_key = ENV['60fcc4988eac07fdaf5aed052aab45c6e8eaadc6']
   end
